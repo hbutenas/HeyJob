fbe848c7b2671685133e3296521365257c28e1d4
Henrikas Butenas <butenashenrikas@gmail.com>
2023-09-28 21:23:32 +0300

    line split, easier to read code

--- routes/Api/V1/Auth/AuthRoutes.php
+++ routes/Api/V1/Auth/AuthRoutes.php
@@ -4,7 +4,17 @@ use App\Http\Controllers\Api\V1\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::group(['prefix' => 'v1/auth'], function () {
+Route::group([
+  'prefix' => 'v1/auth'
+], function () {
   Route::post('register', [AuthController::class, 'register']);
   Route::post('login', [AuthController::class, 'login']);
+});
+
+
+Route::group([
+  'prefix' => 'v1/auth',
+  'middleware' => ['auth:sanctum']
+], function () {
+  Route::post('logout', [AuthController::class, 'logout']);
 });
