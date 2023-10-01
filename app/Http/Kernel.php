7885511eb79883867f88b755d531b6aa7e3e6a23
Henrikas Butenas <butenashenrikas@gmail.com>
2023-10-01 20:42:57 +0300

    new middleware registrated

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,4 +65,8 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
+
+    protected $routeMiddleware = [
+        'validateType' => \App\Http\Middleware\Api\V1\Auth\ValidateType::class
+    ];
 }
