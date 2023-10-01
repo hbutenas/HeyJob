1bebd379966f96951aa8605e619ab88f2fbe4088
Henrikas Butenas <butenashenrikas@gmail.com>
2023-10-01 21:17:55 +0300

    added additional type middleware

--- routes/Api/V1/Profiles/ProfilesRoutes.php
+++ routes/Api/V1/Profiles/ProfilesRoutes.php
@@ -6,7 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([
   'prefix' => 'v1/profile',
-  'middleware' => ['auth:sanctum']
+  'middleware' => [
+    'auth:sanctum',
+    'validateType:job_seeker'
+  ]
 ], function () {
   Route::patch('', [ProfilesController::class, 'update']);
   Route::get('', [ProfilesController::class, 'index']);
