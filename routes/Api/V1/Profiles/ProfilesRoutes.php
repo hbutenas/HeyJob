f7a8f57bd8cfadc9c9b89e0a510568e45f9a82fd
Henrikas Butenas <butenashenrikas@gmail.com>
2023-10-01 20:43:53 +0300

    profile route endpoints declared

---
+++ routes/Api/V1/Profiles/ProfilesRoutes.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Api\V1\Profiles\ProfilesController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::group([
+  'prefix' => 'v1/profile',
+  'middleware' => ['auth:sanctum']
+], function () {
+  Route::patch('', [ProfilesController::class, 'update']);
+  Route::get('', [ProfilesController::class, 'index']);
+  Route::get('{id}', [ProfilesController::class, 'show']);
+});
