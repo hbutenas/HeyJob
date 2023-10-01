4674cf5565523b76e09a33fe7e00532e1165e87a
Henrikas Butenas <butenashenrikas@gmail.com>
2023-10-01 20:44:03 +0300

    company route endpoints declared

---
+++ routes/Api/V1/Companies/CompaniesRoutes.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Api\V1\Companies\CompaniesController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::group([
+  'prefix' => 'v1/company',
+  'middleware' => [
+    'auth:sanctum',
+    'validateType:employer'
+  ]
+], function () {
+  Route::patch('', [CompaniesController::class, 'update']);
+});
