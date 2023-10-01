7a742ab7029e495c0eeeeb6693f2a2f55e117aac
Henrikas Butenas <butenashenrikas@gmail.com>
2023-10-01 21:18:33 +0300

    profiles repository created

---
+++ app/Http/Repositories/Api/V1/Profiles/ProfilesRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Repositories\Api\V1\Profiles;
+
+use App\Models\Profile;
+
+class ProfilesRepository
+{
+  public function update(array $request, int $userId): int
+  {
+    return Profile::where('user_id', $userId)->update($request);
+  }
+
+  public function index(int $userId)
+  {
+    return Profile::where('user_id', $userId)->first();
+  }
+
+  public function show(int $profileId): object
+  {
+    return Profile::where('id', $profileId)->first();
+  }
+}
