82a7c9a4a05fa4dc1071d1879c5629d5faa8c1e3
Henrikas Butenas <butenashenrikas@gmail.com>
2023-10-03 20:13:30 +0300

    removed enums, because will use seeder to seed the table with default values on migration

--- database/migrations/2023_09_14_183426_create_categories_table.php
+++ database/migrations/2023_09_14_183426_create_categories_table.php
@@ -13,40 +13,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->enum('category', [
-                'Service industry',
-                'Production / Manufacturing',
-                'Information technology',
-                'Finance / Accounting',
-                'Organisation and management',
-                'Sales',
-                'Administration / Secretarial',
-                'Technical Engineering',
-                'Transport / Logistics',
-                'Banking / Insurance',
-                'Trade / Purchase / Supply',
-                'Construction / Real estate',
-                'Electronics / Telecommunication',
-                'Human resources',
-                'Energetics / Eletricity',
-                'Law / Legal aid',
-                'Marketing / Advertising',
-                'State and public administration',
-                'Health care / Social care',
-                'Quality assurance',
-                'Tourism, Hotels, Catering',
-                'Education / Science',
-                'Media / Public relations',
-                'Culture / Arts / Entertainment / Sport',
-                'Agriculture / Environmnetal services',
-                'Pharmacy',
-                'Security / Rescue services',
-                'Forestry / Woodworking',
-                'Third sector / NGO',
-                'Temporary / Seasonal',
-                'Voluntary job',
-                'Other'
-            ]);
+            $table->string('category');
             $table->timestamps();
         });
     }
