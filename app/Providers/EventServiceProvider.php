a879d32111fadb084a42a4ee438b19bda31cab18
Henrikas Butenas <butenashenrikas@gmail.com>
2023-10-10 19:46:33 +0300

    Custom events added to provider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Events\Api\V1\Auth\CompanyProfileCreated;
+use App\Events\Api\V1\Auth\UserProfileCreated;
+use App\Listeners\Api\V1\Auth\CreateCompanyProfile;
+use App\Listeners\Api\V1\Auth\CreateUserProfile;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -18,6 +22,12 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        UserProfileCreated::class => [
+            CreateUserProfile::class
+        ],
+        CompanyProfileCreated::class => [
+            CreateCompanyProfile::class
+        ]
     ];
 
     /**
