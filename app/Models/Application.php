2536ecd284b9407b13489c6c1e2fc239c88584c9
Henrikas Butenas <butenashenrikas@gmail.com>
2023-11-16 19:09:41 +0200

    Relations created with user and job models, defined fillable properties

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Application extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'job_id'
+    ];
+
+    public function job()
+    {
+        return $this->belongsTo(Job::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
