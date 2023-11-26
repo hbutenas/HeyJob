f2da1d79a4bb55a0ba3578f78d67e038306de060
Henrikas Butenas <butenashenrikas@gmail.com>
2023-11-26 19:07:36 +0200

    workaround for registration issue, fixed bug

--- client/components/Auth/RegisterComponent.vue
+++ client/components/Auth/RegisterComponent.vue
@@ -10,8 +10,8 @@
         Email address
       </label>
 
-      <input class="border rounded w-full py-2 px-3 mt-1 text-gray-700 leading-tight focus:outline-none"
-             id="email" type="email" placeholder="Email" v-model="email" required>
+      <input class="border rounded w-full py-2 px-3 mt-1 text-gray-700 leading-tight focus:outline-none" id="email"
+        type="email" placeholder="Email" v-model="email" required>
     </div>
 
     <!-- Password field -->
@@ -20,9 +20,8 @@
         Password
       </label>
 
-      <input
-          class="border rounded w-full py-2 px-3 mt-1 text-gray-700 leading-tight focus:outline-none"
-          id="password" type="password" placeholder="******************" v-model="password" required>
+      <input class="border rounded w-full py-2 px-3 mt-1 text-gray-700 leading-tight focus:outline-none" id="password"
+        type="password" placeholder="******************" v-model="password" required>
     </div>
 
     <!-- Confirm password field -->
@@ -31,9 +30,8 @@
         Confirm Password
       </label>
 
-      <input
-          class="border rounded w-full py-2 px-3 mt-1 text-gray-700 leading-tight focus:outline-none"
-          id="confirm_password" type="password" placeholder="******************" v-model="confirmedPassword" required>
+      <input class="border rounded w-full py-2 px-3 mt-1 text-gray-700 leading-tight focus:outline-none"
+        id="confirm_password" type="password" placeholder="******************" v-model="confirmedPassword" required>
     </div>
 
     <!-- Type selection -->
@@ -43,7 +41,7 @@
       </label>
 
       <select v-model="userType" name="types" id="user-types"
-              class="w-full border rounded py-1 px-2 mt-1 text-gray-700 mb-3">
+        class="w-full border rounded py-1 px-2 mt-1 text-gray-700 mb-3">
         <option value="job_seeker">Job seeker</option>
         <option value="employer">Employer</option>
       </select>
@@ -52,8 +50,8 @@
     <!-- Register button -->
     <div class="flex items-center justify-between">
       <button
-          class="bg-white hover:bg-gray-100 text-alternatePrimary font-medium w-full font-semiboldl tracking-wider text-sm py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="submit">
+        class="bg-white hover:bg-gray-100 text-alternatePrimary font-medium w-full font-semiboldl tracking-wider text-sm py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        type="submit">
         Register
       </button>
     </div>
@@ -67,29 +65,28 @@
 
     <!-- Sign in with Google -->
     <button
-        class="flex items-center justify-center bg-white hover:bg-gray-100 font-medium py-2 px-4 mb-4 rounded focus:outline-none focus:shadow-outline w-full">
-                    <span class="mr-2">
-                        <img class="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy"
-                             alt="google logo">
-                    </span>
+      class="flex items-center justify-center bg-white hover:bg-gray-100 font-medium py-2 px-4 mb-4 rounded focus:outline-none focus:shadow-outline w-full">
+      <span class="mr-2">
+        <img class="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo">
+      </span>
       Sign in with Google
     </button>
 
     <!-- Sign in with Facebook  -->
     <button
-        class="flex items-center justify-center bg-white hover:bg-gray-100 font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full">
-                    <span class="mr-2">
-                        <img class="w-6 h-6" src="https://www.svgrepo.com/show/475647/facebook-color.svg" loading="lazy"
-                             alt="google logo">
-                    </span>
+      class="flex items-center justify-center bg-white hover:bg-gray-100 font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full">
+      <span class="mr-2">
+        <img class="w-6 h-6" src="https://www.svgrepo.com/show/475647/facebook-color.svg" loading="lazy"
+          alt="google logo">
+      </span>
       Continue with Facebook
     </button>
   </AuthComponent>
 </template>
 
 <script setup>
-import {ref} from "vue";
-import {useAuthStore} from "~/stores/useAuthStore";
+import { ref } from "vue";
+import { useAuthStore } from "~/stores/useAuthStore";
 
 const auth = useAuthStore();
 const email = ref('');
@@ -98,6 +95,7 @@ const confirmedPassword = ref('');
 const userType = ref('job_seeker');
 const registrationFailed = ref(false);
 const registrationFailedMessage = ref('Please choose an option from the list');
+
 const handleSubmitButton = async () => {
 
   // validate option value
@@ -118,22 +116,26 @@ const handleSubmitButton = async () => {
     type: userType.value,
   });
 
-  // create new user
-  const {error: {value: {data: {errors}}}} = await auth.register(userData.value);
-  // failed to register user due errors
+  const response = await auth.register(userData.value);
 
+  // checks if response.error.value exists and is not null before attempting to read the data property, preventing the TypeError issue
+  const errors = response.error.value && response.error.value.data ? response.error.value.data.errors : false;
+
+  // error occured while creating new users  
   if (errors) {
-    // loop through errors and display the error
     Object.keys(errors).forEach(error => {
       registrationFailed.value = true;
       registrationFailedMessage.value = errors[error][0];
       password.value = '';
       confirmedPassword.value = '';
     });
-  } else {
-    // successfully created new user, redirect to dashboard
-    return navigateTo('/dashboard');
+    return;
   }
+  
+  await auth.login(userData.value);
+
+  return navigateTo('/dashboard');
+
 }
 
 
